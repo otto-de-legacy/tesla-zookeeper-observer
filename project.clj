58a7324fe440819d210a028503a4d77e65dd198f
Christian Stamm <christian.stamm@otto.de>
2015-06-12 18:20:31 +0200

    moved to tesla-microservice 0.1.15

--- project.clj
+++ project.clj
@@ -6,6 +6,12 @@
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-zookeeper-observer"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [de.otto/tesla-microservice "0.1.12"]
-                           [zookeeper-clj "0.9.3"]])
+                           [zookeeper-clj "0.9.3"]]
+
+            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.15"]
+                                                 [com.stuartsierra/component "0.2.3"]]}
+                       :dev      {:dependencies [[org.slf4j/slf4j-api "1.7.12"]
+                                                 [ch.qos.logback/logback-core "1.1.3"]
+                                                 [ch.qos.logback/logback-classic "1.1.3"]]
+                                  :plugins      [[lein-ancient "0.6.7"]]}})
 
