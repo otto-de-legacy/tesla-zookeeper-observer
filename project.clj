587881c6d8e022da0fdfa6a021d7de5922159d58
Manan <manan.bharara@gmail.com>
2016-01-12 14:48:52 +0100

    mb/cd| Bumping up clojure and dependency versions

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
                       :url  "http://www.apache.org/license/LICENSE-2.0.html"}
             :scm {:name "git"
                   :url  "https://github.com/otto-de/tesla-zookeeper-observer"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [zookeeper-clj "0.9.3"]]
 
-            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.18"]
-                                                 [com.stuartsierra/component "0.2.3"]]}
-                       :dev      {:dependencies [[org.slf4j/slf4j-api "1.7.12"]
+            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.25"]
+                                                 [com.stuartsierra/component "0.3.1"]]}
+                       :dev      {:dependencies [[org.slf4j/slf4j-api "1.7.13"]
                                                  [ch.qos.logback/logback-core "1.1.3"]
                                                  [ch.qos.logback/logback-classic "1.1.3"]]
                                   :plugins      [[lein-ancient "0.6.7"]]}})
