fd4aa4640d3913bb382669b14b2e0aa65e6823d6
carl.duevel <carl.duevel@holisticon.de>
2016-10-13 19:00:03 +0200

    Added release config

--- project.clj
+++ project.clj
@@ -7,9 +7,7 @@
         :url  "https://github.com/otto-de/tesla-zookeeper-observer"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [zookeeper-clj "0.9.3"]]
-  :lein-release {:scm        :git
-                 :deploy-via :shell
-                 :shell      ["lein" "deploy" "clojars"]}
+  :lein-release {:deploy-via :clojars}
   :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.32"]
                                        [com.stuartsierra/component "0.3.1"]]}
              :dev      {:dependencies [[org.slf4j/slf4j-api "1.7.13"]
